d5b852a0b18bb12dd182a61d5a2f15c2b67646ae
LourensWalters <48653822+LourensWalters@users.noreply.github.com>
2021-05-14 14:03:45 +0100

    Initial upload. Not reviewed

---
+++ auto-mpg/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting fuel consumption in miles per gallon.',
+    author='Lourens Walters',
+    license='MIT',
+)
