148f305df8b67e42b1beef4ad69ca8115ae9662b
LourensWalters <48653822+LourensWalters@users.noreply.github.com>
2021-05-16 07:12:16 +0100

    Initial upload

---
+++ california-housing/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The objective of the analysis is to predict median house prices in different regions of California as per the 1990 census data.',
+    author='Lourens Walters',
+    license='MIT',
+)
